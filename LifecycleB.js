cac1dac86824ff1f35c04fdc289c314438564443
rajeshwaridev <123530718+rajeshwaridev@users.noreply.github.com>
2023-08-17 05:49:50 +0530

    Add files via upload

---
+++ LifecycleB.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class LifecycleB extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Raji'
+    }
+    console.log('LifecycleB constructor')
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('LifecycleB getDerivedStateFromProps')
+    return null
+  }
+
+  componentDidMount() {
+    console.log('LifecycleB componentDidMount')
+  }
+  shouldComponentUpdate(){
+    console.log('LifecycleB Render(should)')
+    return true
+}
+componentDidUpdate(){
+    console.log('Lifecycle B component did update')
+}
+getSnapshotBeforeUpdate(){
+    console.log('Lifecycle B snapshot before update')
+    return null
+}
+  render() {
+    console.log('LifecycleB render')
+    return (
+      <div>
+        LifecycleB
+      </div>
+    )
+  }
+}
+
+export default LifecycleB
