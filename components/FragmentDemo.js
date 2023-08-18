7ac657b81ce123ce0f216b821c55b96f9070b575
rajeshwaridev <123530718+rajeshwaridev@users.noreply.github.com>
2023-08-18 18:02:28 +0530

    Add files via upload

---
+++ components/FragmentDemo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FragmentDemo() {
+  return (
+    <React.Fragment>
+      <h1>FragmentDemo</h1>
+      <p>Welcome to my website</p>
+    </React.Fragment>   
+  )
+}
+
+export default FragmentDemo
