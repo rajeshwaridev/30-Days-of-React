4d8fe54528eab63a924fa4fffb2b687f4b9033ae
rajeshwaridev <123530718+rajeshwaridev@users.noreply.github.com>
2023-08-20 15:56:26 +0530

    Add files via upload

---
+++ components/HoverCounter2.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+ class HoverCounter2 extends Component {
+    // constructor(props) {
+    //     super(props)
+      
+    //     this.state = {
+    //        count:0
+    //     }
+    //   }
+    //   incrementCount=()=>{
+    //       this.setState(prevState=>{
+    //           return {count:prevState.count+1}
+    //       })
+    //   }
+  render() {
+    const {count,incrementCount}=this.props
+    return (
+      
+        <h2 onMouseOver={incrementCount}>Hover {count} times</h2>
+
+    )
+  }
+}
+
+export default HoverCounter2
